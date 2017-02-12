998dd87c83648cc1c4f96d98f885bfadfae90860
zzingobomi <zzingo5@naver.com>
2017-02-13 00:24:27 +0900

    Initial commit

---
+++ app/src/main/java/com/zzingobomi/moviegame/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zzingobomi.moviegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // MovieGame 의 직접적인 구현은 여기서 합니다.
+    }
+}
