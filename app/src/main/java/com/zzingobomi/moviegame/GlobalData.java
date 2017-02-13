4e2775288287b1e42530563a9e04597c41af30ca
zzingobomi <zzingo5@naver.com>
2017-02-13 20:52:57 +0900

    start MovieGame Project

--- app/src/main/java/com/zzingobomi/moviegame/GlobalData.java
+++ app/src/main/java/com/zzingobomi/moviegame/GlobalData.java
@@ -3,6 +3,14 @@ package com.zzingobomi.moviegame;
 /**
  * Created by JongChan on 2017-02-13.
  * MovieGame 의 모든 전역 설정 및 변수들을 여기에 선언합니다.
+ * 1. 동영상 FullScreen 재생
+ *
+ */
+
+/**
+ * 화면에 보여주는 방식은 처음에는 MainActivity 에서 하나의 VideoView 로 보여주자.
+ * 후에 가능하다면 더블 퍼버링 방식으로 FragmentActivity 를 이용해서 번갈아가며 보여주기 방식
+ * 으로 개선 가능하다면 개선
  */
 
  /**
--- app/src/main/java/com/zzingobomi/moviegame/MainActivity.java
+++ app/src/main/java/com/zzingobomi/moviegame/MainActivity.java
@@ -1,10 +1,33 @@
 package com.zzingobomi.moviegame;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
 {
+    VideoView               mVideoView;
+    MediaController         mMediaController;
+
+
+    // Test
+    String schema   = "http";
+    String ip       = "59.16.152.204";
+    String port     = "5005";
+    String id       = "testvideo";
+    String pw       = "info3775";
+    String dir      = "testvideo";
+    String fileName = "testvideo2.mp4";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,6 +35,64 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // MovieGame 의 직접적인 구현은 여기서 합니다.
+        // 레이아웃 위젯
+        mVideoView = (VideoView)this.findViewById(R.id.videoview);
+
+        // 미디어 컨트롤러 추가
+        mMediaController = new MediaController(MainActivity.this);
+        mVideoView.setMediaController(mMediaController);
+
+        // 비디오 뷰 포커스 요청
+        mVideoView.requestFocus();
+
+        Uri uri = Uri.parse(schema + "://" + ip + ":" + port + "/" + dir + "/" + fileName);
+        try
+        {
+            // http id, pw 인증
+            Method setVideoURIMetod = mVideoView.getClass().getMethod("setVideoURI", Uri.class, Map.class);
+            Map<String, String> header = new HashMap<String, String>(1);
+            final String cred = id + ":" + pw;
+            final String auth = "Basic " + Base64.encodeToString(cred.getBytes(), Base64.URL_SAFE|Base64.NO_WRAP);
+            header.put("Authorization", auth);
+
+            // VideoView 에 Uri 설정
+            setVideoURIMetod.invoke(mVideoView, uri, header);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        //동영상이 재생준비가 완료되었을때 호출되는 리스너
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener()
+        {
+            @Override
+            public void onPrepared(MediaPlayer mp)
+            {
+                //Toast.makeText(getApplicationContext(), "onPrepared", Toast.LENGTH_SHORT).show();
+                playVideo();
+            }
+        });
+
+        // 동영상 재생이 완료되었을 때
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+        {
+            @Override
+            public void onCompletion(MediaPlayer mp)
+            {
+                //Toast.makeText(getApplicationContext(), "onCompletion", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void playVideo()
+    {
+        mVideoView.seekTo(0);
+        mVideoView.start();
+    }
+
+    private void stopVideo()
+    {
+        mVideoView.pause();
     }
 }
