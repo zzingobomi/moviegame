b7606fa136bab21d6616cfa191a36efecc612d28
zzingobomi <zzingo5@naver.com>
2017-02-13 20:05:45 +0900

    start MovieGame Project

--- app/src/main/java/com/zzingobomi/moviegame/GlobalData.java
+++ app/src/main/java/com/zzingobomi/moviegame/GlobalData.java
@@ -8,12 +8,13 @@ package com.zzingobomi.moviegame;
  /**
  * NextFile 방식은 LocalData 방식
  * SQLite 를 이용해서 DB 에다가 정보를 저장하고 여기서 읽어오는게 좋을듯 ( SQLIte 를 좀 연구 ) DB 구조 짜야함
-  * Index, FileName, BtnType,
+  * Index, FileName, BtnType, Next1, Next2, Next3, Next4
  */
 
 /**
  * 버튼 레이아웃은 여러가지 버튼의 유형을 미리 만든 후 각 챕터마다 버튼 유형을 선택?
  * 약간 어설픈 면은 있으나 이게 가장 현실적이면서 가능한 구조일듯..
+ * 가로로 '2', '3', '4', '+' 이렇게 4개의 유형을 만든다
  */
 
 public class GlobalData
